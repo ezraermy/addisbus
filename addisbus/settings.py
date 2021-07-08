1fc6f2b83232f04ef1ac5ec7964e1c6e668aa674
ezraermy <ezraermy@gmail.com>
2021-07-08 11:20:39 +0300

    Addess added to setting

--- addisbus/settings.py
+++ addisbus/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'qek-b$u=75(se^u^)-ti!xo+t@#lob
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ezra-addisbus.herokuapp.com']
 
 
 # Application definition
@@ -139,3 +139,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
